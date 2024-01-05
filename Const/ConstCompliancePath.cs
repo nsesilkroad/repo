e11642af0a962ad8a9a1c8560f0fae331db32058
Sangyong Shin <syshin@nsetec.com>
2024-01-05 09:42:46 +0900

    Add 2 Files

---
+++ Const/ConstCompliancePath.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Silkroad.Modules.ComplianceManagement.Const
+{
+    /// <summary>
+    ///     Compliance 문서 관련 경로
+    /// </summary>
+    public class ConstCompliancePath
+    {
+        public static readonly string CurrentDirectorySymbol = ".\\";
+        public static readonly string TemplatePath = "ComplianceTemplate\\";
+        public static readonly string TrackerPath = "Attachment\\Compliance\\";
+    }
+}
