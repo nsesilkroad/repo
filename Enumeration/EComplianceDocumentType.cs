f1d66be0742412ac2c4210aa3cda0c028c523c93
Sangyong Shin <syshin@nsetec.com>
2024-01-05 09:39:46 +0900

    Modified Enum

--- Enumeration/EComplianceDocumentType.cs
+++ Enumeration/EComplianceDocumentType.cs
@@ -10,13 +10,4 @@ namespace Silkroad.Modules.ComplianceManagement.Enumeration
         [EnumStringValue("pdf")] Pdf = 0,
         [EnumStringValue("docx")] Docx
     }
-
-    /// <summary>
-    ///     Compliance 등록문서 종류
-    /// </summary>
-    public enum EComplianceDocumentSize
-    {
-        [EnumStringValue("requirement")] Req = 0,
-        [EnumStringValue("design")] Design
-    }
 }
